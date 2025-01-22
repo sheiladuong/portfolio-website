19e9f32ed84ca392a22a1df7d87982d472171f0f
Sheila Duong <sheiladuong235@gmail.com>
2025-01-21 22:11:43 -0500

    Created src/components/ folder and Navbar.jsx

---
+++ portfolio-website/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <nav className='navbar'>
+            <a href='/'>s.d.</a>
+            <div className='nav-links'>
+                <a href='./#projects'>projects</a>
+                <a href='/Resume'>resume</a>
+                <a href='./#connect'>connect</a>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
