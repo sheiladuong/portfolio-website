60e099000cb7da5f6679528dab2ac1b341a8eaf0
Sheila Duong <sheiladuong235@gmail.com>
2025-01-30 14:53:47 -0500

    Building AboutMe.jsx component

--- portfolio-website/src/App.jsx
+++ portfolio-website/src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from './components/Navbar.jsx'
+import AboutMe from './components/AboutMe.jsx'
 import './App.css'
 
 function App() {
   return (
     <>
       <Navbar />
+      <AboutMe />
     </>
   )
 }
---
+++ portfolio-website/src/components/AboutMe.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function AboutMe() {
+    return (
+        <>
+        <div className='aboutme-container'>
+            <div className='title-section'>
+                <h2 className='name'>SHEILA DUONG</h2>
+                <h1 className='title'>FRONT <span className="script">end</span><br />DEVELOPER</h1>
+            </div>
+            <div className='headshot-section'>
+                <img src='./assets/placeholder.jpg' alt='Sheila Duong'></img>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default AboutMe
