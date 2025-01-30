7eb3c2632234c36b4b2a3bfc9329213f2f9d7f32
Sheila Duong <sheiladuong235@gmail.com>
2025-01-30 12:42:00 -0500

    Modified

--- portfolio-website/src/App.jsx
+++ portfolio-website/src/App.jsx
@@ -1,12 +1,7 @@
-import { useEffect } from 'react'
 import Navbar from './components/Navbar.jsx'
 import './App.css'
 
 function App() {
-  useEffect(() => {
-    document.title = "Portfolio | Sheila Duong"; // Change this to your desired title
-  }, []);
-
   return (
     <>
       <Navbar />
