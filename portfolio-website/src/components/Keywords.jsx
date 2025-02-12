c7cd38526689c2ce45de59664f48d17483919713
Sheila Duong <sheiladuong235@gmail.com>
2025-02-11 22:20:01 -0500

    Modified quotations

--- portfolio-website/src/components/Keywords.jsx
+++ portfolio-website/src/components/Keywords.jsx
@@ -6,22 +6,22 @@ function Keywords() {
         'Vite', 'Swift', 'SwiftUI', 'Java', 'Python', 'PHP', 'MySQL'];
 
     useEffect(() => {
-        const scrollers = document.querySelectorAll(".scroller");
+        const scrollers = document.querySelectorAll('.scroller');
     
-        if (!window.matchMedia("(prefers-reduced-motion: reduce").matches) {
+        if (!window.matchMedia('(prefers-reduced-motion: reduce)').matches) {
             scrollAnimation();
         }
     
         function scrollAnimation() {
             scrollers.forEach((scroller) => {
-                scroller.setAttribute("data-animated", true);
+                scroller.setAttribute('data-animated', true);
         
-                const scrollerItems = scroller.querySelector(".scroller-items");
+                const scrollerItems = scroller.querySelector('.scroller-items');
                 const scrollerContent = Array.from(scrollerItems.children);
     
                 scrollerContent.forEach((item) => {
                     const duplicatedItem = item.cloneNode(true);
-                    duplicatedItem.setAttribute("aria-hidden", true);
+                    duplicatedItem.setAttribute('aria-hidden', true);
                     scrollerItems.appendChild(duplicatedItem);
                 });
             });
@@ -30,8 +30,8 @@ function Keywords() {
         
     return (
         <>
-        <div className="scroller" data-direction="right">
-            <ul className="scroller-items">
+        <div className='scroller' data-direction='right'>
+            <ul className='scroller-items'>
                 {keywords.map((keyword) => (
                     <li key={keyword}>{keyword}</li>
                 ))}
