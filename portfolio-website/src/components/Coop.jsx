56f4d1442343a4bba6e61fea2bbab4faf8c95bab
Sheila Duong <sheiladuong235@gmail.com>
2025-02-11 22:47:43 -0500

    Completed Coop.jsx

---
+++ portfolio-website/src/components/Coop.jsx
@@ -0,0 +1,18 @@
+function Coop() {
+
+    return (
+        <>
+            <div className='coop-container'>
+                <p>
+                    I'm an undergraduate Web and Mobile Computing student at 
+                    Rochester Institute of Technology. I'm seeking a co-op/internship 
+                    for Summer 2025.
+                </p>
+                <button>VIEW RESUME</button>
+            </div>
+        </>
+    )
+
+}
+
+export default Coop
